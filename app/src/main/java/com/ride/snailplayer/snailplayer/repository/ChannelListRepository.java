f52ef95b86a5f913c4dd38f3bc285102608bbff7
Stormouble <stormouble@gmail.com>
2017-05-24 22:39:13 +0800

    添加ChannelList的相关类

---
+++ app/src/main/java/com/ride/snailplayer/snailplayer/repository/ChannelListRepository.java
@@ -0,0 +1,9 @@
+package com.ride.snailplayer.snailplayer.repository;
+
+/**
+ * @author Stormouble
+ * @since 2017/5/24.
+ */
+
+public class ChannelListRepository {
+}
---
+++ app/src/main/java/com/ride/snailplayer/snailplayer/ui/home/ChannelListViewModel.java
@@ -0,0 +1,11 @@
+package com.ride.snailplayer.snailplayer.ui.home;
+
+import android.arch.lifecycle.ViewModel;
+
+/**
+ * @author Stormouble
+ * @since 2017/5/24.
+ */
+
+public class ChannelListViewModel extends ViewModel {
+}
