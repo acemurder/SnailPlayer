f8360d2db06632480b416eae0a93e968fcf68e54
acemurder <vqqs@163.com>
2017-06-13 15:36:29 +0800

    add

---
+++ app/src/main/java/com/ride/snailplayer/widget/VideoPlayGestureController.java
@@ -0,0 +1,12 @@
+package com.ride.snailplayer.widget;
+
+/**
+ * Created by ：AceMurder
+ * Created on ：2017/6/13
+ * Created for : snailplayer.
+ * Enjoy it !!!
+ */
+
+public class VideoPlayGestureController  {
+
+}
--- app/src/main/java/com/ride/snailplayer/widget/VideoPlayView.java
+++ app/src/main/java/com/ride/snailplayer/widget/VideoPlayView.java
@@ -6,7 +6,9 @@ import android.content.pm.ActivityInfo;
 import android.os.Handler;
 import android.os.Message;
 import android.util.AttributeSet;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -277,4 +279,63 @@ public class VideoPlayView extends RelativeLayout {
     public QiyiVideoView getmVideoView() {
         return mVideoView;
     }
+
+    //private GestureDetector mGestureDetector = new GestureDetector()
+
+    private class VideoGestureListener extends GestureDetector.SimpleOnGestureListener{
+        public VideoGestureListener() {
+            super();
+        }
+
+        @Override
+        public boolean onSingleTapUp(MotionEvent e) {
+            return super.onSingleTapUp(e);
+        }
+
+        @Override
+        public void onLongPress(MotionEvent e) {
+            super.onLongPress(e);
+        }
+
+        @Override
+        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+            return super.onScroll(e1, e2, distanceX, distanceY);
+        }
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            return super.onFling(e1, e2, velocityX, velocityY);
+        }
+
+        @Override
+        public void onShowPress(MotionEvent e) {
+            super.onShowPress(e);
+        }
+
+        @Override
+        public boolean onDown(MotionEvent e) {
+            return super.onDown(e);
+        }
+
+        @Override
+        public boolean onDoubleTap(MotionEvent e) {
+            return super.onDoubleTap(e);
+        }
+
+        @Override
+        public boolean onDoubleTapEvent(MotionEvent e) {
+            return super.onDoubleTapEvent(e);
+        }
+
+        @Override
+        public boolean onSingleTapConfirmed(MotionEvent e) {
+            return super.onSingleTapConfirmed(e);
+        }
+
+        @Override
+        public boolean onContextClick(MotionEvent e) {
+            return super.onContextClick(e);
+        }
+    }
+
 }
